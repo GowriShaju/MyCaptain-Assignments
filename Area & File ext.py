167307c0bfd956198508cbcc50af47f17e16a880
GowriShaju <98249161+GowriShaju@users.noreply.github.com>
2022-02-06 11:46:40 +0530

    Add files via upload

---
+++ Area & File ext.py
@@ -0,0 +1,12 @@
+#1 area of the circle
+r = float(input("Radius of the circle:"))
+pi = 3.14
+A = pi*r**2
+print("Area of the circle:",A)
+
+
+#2 file extension 
+fn = input("filename:")
+fn_ext = fn.split(".")[-1]
+print("extension:",fn_ext)
+
