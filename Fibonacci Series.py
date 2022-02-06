fb4ed2de32d0e8e1a238c851e90b152ec61cd1c2
GowriShaju <98249161+GowriShaju@users.noreply.github.com>
2022-02-06 11:19:54 +0530

    Add files via upload

---
+++ Fibonacci Series.py
@@ -0,0 +1,15 @@
+#fibonacci series
+
+n = int(input("no. of terms:"))
+a = 0
+b = 1
+if n <= 0:
+    print(a)
+else:
+    print(a,b,end=' ')
+    for i in range(2,n):
+        c = a+b
+        print(c,end=' ')
+        a = b
+        b = c
+
